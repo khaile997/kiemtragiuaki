86ce35b5325e74185db598f5ac7e0c3d3629076e
khaile <lvkhai16064@cusc.ctu.edu.vn>
2019-10-28 21:22:39 +0700

    tao form nhap lieu

---
+++ Kiemtragiuaki/Frmnhaplieutiso.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Kiemtragiuaki
+{
+    partial class Frmnhaplieutiso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnxemkq = new System.Windows.Forms.Button();
+            this.btnluu = new System.Windows.Forms.Button();
+            this.txtbang = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDoiA = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDoiB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTisoA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTisoB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::Kiemtragiuaki.Properties.Resources.icc_t20;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(543, 320);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Controls.Add(this.btnxemkq);
+            this.panel2.Controls.Add(this.btnluu);
+            this.panel2.Controls.Add(this.txtbang);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.txtDoiA);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.txtDoiB);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.txtTisoA);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.txtTisoB);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(90, 37);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(358, 236);
+            this.panel2.TabIndex = 0;
+            // 
+            // btnxemkq
+            // 
+            this.btnxemkq.Location = new System.Drawing.Point(203, 195);
+            this.btnxemkq.Name = "btnxemkq";
+            this.btnxemkq.Size = new System.Drawing.Size(82, 23);
+            this.btnxemkq.TabIndex = 13;
+            this.btnxemkq.Text = "Xem Kết Quả";
+            this.btnxemkq.UseVisualStyleBackColor = true;
+            // 
+            // btnluu
+            // 
+            this.btnluu.Location = new System.Drawing.Point(76, 195);
+            this.btnluu.Name = "btnluu";
+            this.btnluu.Size = new System.Drawing.Size(86, 23);
+            this.btnluu.TabIndex = 14;
+            this.btnluu.Text = "Lưu";
+            this.btnluu.UseVisualStyleBackColor = true;
+            // 
+            // txtbang
+            // 
+            this.txtbang.Location = new System.Drawing.Point(144, 27);
+            this.txtbang.Name = "txtbang";
+            this.txtbang.Size = new System.Drawing.Size(63, 20);
+            this.txtbang.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(247, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Tỉ Số Đội B";
+            // 
+            // txtDoiA
+            // 
+            this.txtDoiA.Location = new System.Drawing.Point(4, 79);
+            this.txtDoiA.Name = "txtDoiA";
+            this.txtDoiA.Size = new System.Drawing.Size(121, 20);
+            this.txtDoiA.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Tỉ Số Đội A";
+            // 
+            // txtDoiB
+            // 
+            this.txtDoiB.Location = new System.Drawing.Point(231, 79);
+            this.txtDoiB.Name = "txtDoiB";
+            this.txtDoiB.Size = new System.Drawing.Size(123, 20);
+            this.txtDoiB.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(271, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Độ B";
+            // 
+            // txtTisoA
+            // 
+            this.txtTisoA.Location = new System.Drawing.Point(4, 141);
+            this.txtTisoA.Name = "txtTisoA";
+            this.txtTisoA.Size = new System.Drawing.Size(121, 20);
+            this.txtTisoA.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Đội A";
+            // 
+            // txtTisoB
+            // 
+            this.txtTisoB.Location = new System.Drawing.Point(231, 141);
+            this.txtTisoB.Name = "txtTisoB";
+            this.txtTisoB.Size = new System.Drawing.Size(123, 20);
+            this.txtTisoB.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(154, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Bảng";
+            // 
+            // Frmnhaplieutiso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 320);
+            this.Controls.Add(this.panel1);
+            this.Name = "Frmnhaplieutiso";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nhập Liệu Tỉ Số";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnxemkq;
+        private System.Windows.Forms.Button btnluu;
+        private System.Windows.Forms.TextBox txtbang;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDoiA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDoiB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTisoA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTisoB;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Kiemtragiuaki/Frmnhaplieutiso.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kiemtragiuaki
+{
+    public partial class Frmnhaplieutiso : Form
+    {
+        public Frmnhaplieutiso()
+        {
+            InitializeComponent();
+        }
+
+        private void btnluu_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

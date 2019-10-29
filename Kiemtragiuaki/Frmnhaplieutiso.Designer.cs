5745aea6af3cffbdf95e1b92f773e93e31d61ce0
khaile <lvkhai16064@cusc.ctu.edu.vn>
2019-10-29 17:25:35 +0700

    Hoàn thành

--- Kiemtragiuaki/Frmnhaplieutiso.Designer.cs
+++ Kiemtragiuaki/Frmnhaplieutiso.Designer.cs
@@ -53,8 +53,9 @@
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(543, 320);
+            this.panel1.Size = new System.Drawing.Size(724, 394);
             this.panel1.TabIndex = 2;
             // 
             // panel2
@@ -75,34 +76,40 @@
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.txtTisoB);
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Location = new System.Drawing.Point(90, 37);
+            this.panel2.Location = new System.Drawing.Point(120, 46);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(358, 236);
+            this.panel2.Size = new System.Drawing.Size(477, 290);
             this.panel2.TabIndex = 0;
             // 
             // btnxemkq
             // 
-            this.btnxemkq.Location = new System.Drawing.Point(203, 195);
+            this.btnxemkq.Location = new System.Drawing.Point(271, 240);
+            this.btnxemkq.Margin = new System.Windows.Forms.Padding(4);
             this.btnxemkq.Name = "btnxemkq";
-            this.btnxemkq.Size = new System.Drawing.Size(82, 23);
+            this.btnxemkq.Size = new System.Drawing.Size(109, 28);
             this.btnxemkq.TabIndex = 13;
             this.btnxemkq.Text = "Xem Kết Quả";
             this.btnxemkq.UseVisualStyleBackColor = true;
+            this.btnxemkq.Click += new System.EventHandler(this.btnxemkq_Click);
             // 
             // btnluu
             // 
-            this.btnluu.Location = new System.Drawing.Point(76, 195);
+            this.btnluu.Location = new System.Drawing.Point(101, 240);
+            this.btnluu.Margin = new System.Windows.Forms.Padding(4);
             this.btnluu.Name = "btnluu";
-            this.btnluu.Size = new System.Drawing.Size(86, 23);
+            this.btnluu.Size = new System.Drawing.Size(115, 28);
             this.btnluu.TabIndex = 14;
             this.btnluu.Text = "Lưu";
             this.btnluu.UseVisualStyleBackColor = true;
+            this.btnluu.Click += new System.EventHandler(this.btnluu_Click_1);
             // 
             // txtbang
             // 
-            this.txtbang.Location = new System.Drawing.Point(144, 27);
+            this.txtbang.Location = new System.Drawing.Point(192, 33);
+            this.txtbang.Margin = new System.Windows.Forms.Padding(4);
             this.txtbang.Name = "txtbang";
-            this.txtbang.Size = new System.Drawing.Size(63, 20);
+            this.txtbang.Size = new System.Drawing.Size(83, 22);
             this.txtbang.TabIndex = 3;
             // 
             // label5
@@ -110,17 +117,19 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(247, 115);
+            this.label5.Location = new System.Drawing.Point(329, 142);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(86, 16);
+            this.label5.Size = new System.Drawing.Size(106, 20);
             this.label5.TabIndex = 8;
             this.label5.Text = "Tỉ Số Đội B";
             // 
             // txtDoiA
             // 
-            this.txtDoiA.Location = new System.Drawing.Point(4, 79);
+            this.txtDoiA.Location = new System.Drawing.Point(5, 97);
+            this.txtDoiA.Margin = new System.Windows.Forms.Padding(4);
             this.txtDoiA.Name = "txtDoiA";
-            this.txtDoiA.Size = new System.Drawing.Size(121, 20);
+            this.txtDoiA.Size = new System.Drawing.Size(160, 22);
             this.txtDoiA.TabIndex = 4;
             // 
             // label4
@@ -128,17 +137,19 @@
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(22, 115);
+            this.label4.Location = new System.Drawing.Point(29, 142);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(86, 16);
+            this.label4.Size = new System.Drawing.Size(105, 20);
             this.label4.TabIndex = 9;
             this.label4.Text = "Tỉ Số Đội A";
             // 
             // txtDoiB
             // 
-            this.txtDoiB.Location = new System.Drawing.Point(231, 79);
+            this.txtDoiB.Location = new System.Drawing.Point(308, 97);
+            this.txtDoiB.Margin = new System.Windows.Forms.Padding(4);
             this.txtDoiB.Name = "txtDoiB";
-            this.txtDoiB.Size = new System.Drawing.Size(123, 20);
+            this.txtDoiB.Size = new System.Drawing.Size(163, 22);
             this.txtDoiB.TabIndex = 5;
             // 
             // label3
@@ -146,17 +157,19 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(271, 51);
+            this.label3.Location = new System.Drawing.Point(361, 63);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.Size = new System.Drawing.Size(51, 20);
             this.label3.TabIndex = 10;
             this.label3.Text = "Độ B";
             // 
             // txtTisoA
             // 
-            this.txtTisoA.Location = new System.Drawing.Point(4, 141);
+            this.txtTisoA.Location = new System.Drawing.Point(5, 174);
+            this.txtTisoA.Margin = new System.Windows.Forms.Padding(4);
             this.txtTisoA.Name = "txtTisoA";
-            this.txtTisoA.Size = new System.Drawing.Size(121, 20);
+            this.txtTisoA.Size = new System.Drawing.Size(160, 22);
             this.txtTisoA.TabIndex = 6;
             // 
             // label2
@@ -164,38 +177,43 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(43, 51);
+            this.label2.Location = new System.Drawing.Point(57, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.Size = new System.Drawing.Size(55, 20);
             this.label2.TabIndex = 11;
             this.label2.Text = "Đội A";
             // 
             // txtTisoB
             // 
-            this.txtTisoB.Location = new System.Drawing.Point(231, 141);
+            this.txtTisoB.Location = new System.Drawing.Point(308, 174);
+            this.txtTisoB.Margin = new System.Windows.Forms.Padding(4);
             this.txtTisoB.Name = "txtTisoB";
-            this.txtTisoB.Size = new System.Drawing.Size(123, 20);
+            this.txtTisoB.Size = new System.Drawing.Size(163, 22);
             this.txtTisoB.TabIndex = 7;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(154, 8);
+            this.label1.Location = new System.Drawing.Point(205, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(52, 20);
             this.label1.TabIndex = 12;
             this.label1.Text = "Bảng";
             // 
             // Frmnhaplieutiso
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(543, 320);
+            this.ClientSize = new System.Drawing.Size(724, 394);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Frmnhaplieutiso";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Nhập Liệu Tỉ Số";
+            this.Load += new System.EventHandler(this.Frmnhaplieutiso_Load);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
--- Kiemtragiuaki/Frmnhaplieutiso.cs
+++ Kiemtragiuaki/Frmnhaplieutiso.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,9 +18,293 @@ namespace Kiemtragiuaki
             InitializeComponent();
         }
 
-        private void btnluu_Click(object sender, EventArgs e)
+        string connectionstring = Kiemtragiuaki.Properties.Settings.Default.DatabaseBangdiembongdaConnectionString;
+        private void Frmnhaplieutiso_Load(object sender, EventArgs e)
         {
+           
 
         }
+
+        public void themlanmot()
+        {
+            // chuyển kiểu dữ liệu nhập trên textbox thành kiểu int và lưu vào biến tỉ số tương ứng
+            int tisoA = Convert.ToInt32(txtTisoA.Text);
+            int tisoB = Convert.ToInt32(txtTisoB.Text);
+            int hieusoA = 0, hieusoB = 0, diemA = 0, diemB = 0;
+            // lấy tỉ số 2 đội lưu vào biến tỉ số và sau đó dùng biến tỉ số để lưu vào csdl
+            string tisoluot1 = (tisoA + "-" + tisoB).ToString();
+            
+            string tisoluot2 = (tisoB + "-" + tisoA).ToString();
+           
+            // Kiểm tra dữ liệu nhập
+            if (String.IsNullOrEmpty(txtbang.Text) || String.IsNullOrEmpty(txtDoiA.Text) || String.IsNullOrEmpty(txtDoiB.Text)
+                || String.IsNullOrEmpty(txtTisoA.Text) || String.IsNullOrEmpty(txtTisoB.Text))
+            {
+                MessageBox.Show("Vui lòng kiểm tra lại dữ liệu nhập");
+                return;
+            }
+            if (tisoA > tisoB)
+            {
+                hieusoA = tisoA - tisoB;
+                diemA = 3;
+                // Tạo câu lệnh để thực thi đến database
+                string queryString = @"INSERT INTO BangDiemThiDau([TenBang], [TenDoi], [TenDoiDoiThu], [TiSo], [SoTranDaThiDau], [HieuSo], [Diem]) VALUES(@TenBang, @TenDoi, @TenDoiDoiThu, @TiSo, @SoTranDaThiDau, @HieuSo, @Diem)";
+                // Tạo object từ class SqlConnection (dùng để quản lý kết nối đến Database Server)
+                using (SqlConnection connection = new SqlConnection(connectionstring))
+                {
+                    // Tạo object từ class SqlCommand (dùng để quản lý việc thực thi câu lệnh)
+                    using (SqlCommand command = new SqlCommand(queryString, connection))
+                    {
+                        try
+                        {
+                            // Mở kết nối đến Database Server
+                            connection.Open();
+                            // Truyền dữ liệu vào đúng tham số
+                            command.CommandType = CommandType.Text;
+                            command.Parameters.AddWithValue("@TenBang", txtbang.Text);
+                            command.Parameters.AddWithValue("@TenDoi", txtDoiA.Text);
+                            command.Parameters.AddWithValue("@TenDoiDoiThu", txtDoiB.Text);
+                            command.Parameters.AddWithValue("@Tiso", tisoluot1);
+                            command.Parameters.AddWithValue("@SoTranDaThiDau", 1);
+                            command.Parameters.AddWithValue("@HieuSo", hieusoA);
+                            command.Parameters.AddWithValue("@Diem", diemA);
+                            // Thực thi câu lệnh
+                            int effect = command.ExecuteNonQuery();
+                            // Ngắt kết nối đến Database Server
+                            connection.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            // Hiển thị thông báo nếu có lỗi
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+            }
+            else if (tisoB > tisoA)
+            {
+                hieusoB = tisoB - tisoA;
+                diemB = 0;
+                // Tạo câu lệnh để thực thi đến database
+                string queryString = @"INSERT INTO BangDiemThiDau([TenBang], [TenDoi], [TenDoiDoiThu], [TiSo], [SoTranDaThiDau], [HieuSo], [Diem]) VALUES(@TenBang, @TenDoi, @TenDoiDoiThu, @TiSo, @SoTranDaThiDau, @HieuSo, @Diem)";
+                // Tạo object từ class SqlConnection (dùng để quản lý kết nối đến Database Server)
+                using (SqlConnection connection = new SqlConnection(connectionstring))
+                {
+                    // Tạo object từ class SqlCommand (dùng để quản lý việc thực thi câu lệnh)
+                    using (SqlCommand command = new SqlCommand(queryString, connection))
+                    {
+                        try
+                        {
+                            // Mở kết nối đến Database Server
+                            connection.Open();
+                            // Truyền dữ liệu vào đúng tham số
+                            command.CommandType = CommandType.Text;
+                            command.Parameters.AddWithValue("@TenBang", txtbang.Text);
+                            command.Parameters.AddWithValue("@TenDoi", txtDoiA.Text);
+                            command.Parameters.AddWithValue("@TenDoiDoiThu", txtDoiB.Text);
+                            command.Parameters.AddWithValue("@Tiso", tisoluot1);
+                            command.Parameters.AddWithValue("@SoTranDaThiDau", 1);
+                            command.Parameters.AddWithValue("@HieuSo", hieusoB);
+                            command.Parameters.AddWithValue("@Diem", diemB);
+                            // Thực thi câu lệnh
+                            int effect = command.ExecuteNonQuery();
+                            // Ngắt kết nối đến Database Server
+                            connection.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            // Hiển thị thông báo nếu có lỗi
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+            }
+            else if (tisoA == tisoB)
+            {
+                hieusoA = tisoA - tisoB;
+                diemA = 1;
+                // Tạo câu lệnh để thực thi đến database
+                string queryString = @"INSERT INTO BangDiemThiDau([TenBang], [TenDoi], [TenDoiDoiThu], [TiSo], [SoTranDaThiDau], [HieuSo], [Diem]) VALUES(@TenBang, @TenDoi, @TenDoiDoiThu, @TiSo, @SoTranDaThiDau, @HieuSo, @Diem)";
+                // Tạo object từ class SqlConnection (dùng để quản lý kết nối đến Database Server)
+                using (SqlConnection connection = new SqlConnection(connectionstring))
+                {
+                    // Tạo object từ class SqlCommand (dùng để quản lý việc thực thi câu lệnh)
+                    using (SqlCommand command = new SqlCommand(queryString, connection))
+                    {
+                        try
+                        {
+                            // Mở kết nối đến Database Server
+                            connection.Open();
+                            // Truyền dữ liệu vào đúng tham số
+                            command.CommandType = CommandType.Text;
+                            command.Parameters.AddWithValue("@TenBang", txtbang.Text);
+                            command.Parameters.AddWithValue("@TenDoi", txtDoiA.Text);
+                            command.Parameters.AddWithValue("@TenDoiDoiThu", txtDoiB.Text);
+                            command.Parameters.AddWithValue("@Tiso", tisoluot1);
+                            command.Parameters.AddWithValue("@SoTranDaThiDau", 1);
+                            command.Parameters.AddWithValue("@HieuSo", hieusoA);
+                            command.Parameters.AddWithValue("@Diem", diemA);
+                            // Thực thi câu lệnh
+                            int effect = command.ExecuteNonQuery();
+                            // Ngắt kết nối đến Database Server
+                            connection.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            // Hiển thị thông báo nếu có lỗi
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+        public void themlanhai()
+        {
+            // chuyển kiểu dữ liệu nhập trên textbox thành kiểu int và lưu vào biến tỉ số tương ứng
+            int tisoA = Convert.ToInt32(txtTisoA.Text);
+            int tisoB = Convert.ToInt32(txtTisoB.Text);
+            int hieusoA = 0, hieusoB = 0, diemA = 0, diemB = 0;
+            // lấy tỉ số 2 đội lưu vào biến tỉ số và sau đó dùng biến tỉ số để lưu vào csdl
+            string tisoluot1 = (tisoA + "-" + tisoB).ToString();
+           
+            string tisoluot2 = (tisoB + "-" + tisoA).ToString();
+           
+            // Kiểm tra dữ liệu nhập
+            if (String.IsNullOrEmpty(txtbang.Text) || String.IsNullOrEmpty(txtDoiA.Text) || String.IsNullOrEmpty(txtDoiB.Text)
+                || String.IsNullOrEmpty(txtTisoA.Text) || String.IsNullOrEmpty(txtTisoB.Text))
+            {
+                MessageBox.Show("Vui lòng kiểm tra lại dữ liệu nhập");
+                return;
+            }
+            if (tisoA > tisoB)
+            {
+                hieusoA = tisoB - tisoA;
+                diemA = 0;
+                // Tạo câu lệnh để thực thi đến database
+                string queryString = @"INSERT INTO BangDiemThiDau([TenBang], [TenDoi], [TenDoiDoiThu], [TiSo], [SoTranDaThiDau], [HieuSo], [Diem]) VALUES(@TenBang, @TenDoi, @TenDoiDoiThu, @TiSo, @SoTranDaThiDau, @HieuSo, @Diem)";
+                // Tạo object từ class SqlConnection (dùng để quản lý kết nối đến Database Server)
+                using (SqlConnection connection = new SqlConnection(connectionstring))
+                {
+                    // Tạo object từ class SqlCommand (dùng để quản lý việc thực thi câu lệnh)
+                    using (SqlCommand command = new SqlCommand(queryString, connection))
+                    {
+                        try
+                        {
+                            // Mở kết nối đến Database Server
+                            connection.Open();
+                            // Truyền dữ liệu vào đúng tham số
+                            command.CommandType = CommandType.Text;
+                            command.Parameters.AddWithValue("@TenBang", txtbang.Text);
+                            command.Parameters.AddWithValue("@TenDoi", txtDoiB.Text);
+                            command.Parameters.AddWithValue("@TenDoiDoiThu", txtDoiA.Text);
+                            command.Parameters.AddWithValue("@Tiso", tisoluot2);
+                            command.Parameters.AddWithValue("@SoTranDaThiDau", 1);
+                            command.Parameters.AddWithValue("@HieuSo", hieusoA);
+                            command.Parameters.AddWithValue("@Diem", diemA);
+                            // Thực thi câu lệnh
+                            int effect = command.ExecuteNonQuery();
+                            // Ngắt kết nối đến Database Server
+                            connection.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            // Hiển thị thông báo nếu có lỗi
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+            }
+            else if (tisoB > tisoA)
+            {
+                hieusoB = tisoB - tisoA;
+                diemB = 3;
+                // Tạo câu lệnh để thực thi đến database
+                string queryString = @"INSERT INTO BangDiemThiDau([TenBang], [TenDoi], [TenDoiDoiThu], [TiSo], [SoTranDaThiDau], [HieuSo], [Diem]) VALUES(@TenBang, @TenDoi, @TenDoiDoiThu, @TiSo, @SoTranDaThiDau, @HieuSo, @Diem)";
+                // Tạo object từ class SqlConnection (dùng để quản lý kết nối đến Database Server)
+                using (SqlConnection connection = new SqlConnection(connectionstring))
+                {
+                    // Tạo object từ class SqlCommand (dùng để quản lý việc thực thi câu lệnh)
+                    using (SqlCommand command = new SqlCommand(queryString, connection))
+                    {
+                        try
+                        {
+                            // Mở kết nối đến Database Server
+                            connection.Open();
+                            // Truyền dữ liệu vào đúng tham số
+                            command.CommandType = CommandType.Text;
+                            command.Parameters.AddWithValue("@TenBang", txtbang.Text);
+                            command.Parameters.AddWithValue("@TenDoi", txtDoiB.Text);
+                            command.Parameters.AddWithValue("@TenDoiDoiThu", txtDoiA.Text);
+                            command.Parameters.AddWithValue("@Tiso", tisoluot2);
+                            command.Parameters.AddWithValue("@SoTranDaThiDau", 1);
+                            command.Parameters.AddWithValue("@HieuSo", hieusoB);
+                            command.Parameters.AddWithValue("@Diem", diemB);
+                            // Thực thi câu lệnh
+                            int effect = command.ExecuteNonQuery();
+                            // Ngắt kết nối đến Database Server
+                            connection.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            // Hiển thị thông báo nếu có lỗi
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+            }
+            else if (tisoA == tisoB)
+            {
+                hieusoA = tisoA - tisoB;
+                diemA = 1;
+                // Tạo câu lệnh để thực thi đến database
+                string queryString = @"INSERT INTO BangDiemThiDau([TenBang], [TenDoi], [TenDoiDoiThu], [TiSo], [SoTranDaThiDau], [HieuSo], [Diem]) VALUES(@TenBang, @TenDoi, @TenDoiDoiThu, @TiSo, @SoTranDaThiDau, @HieuSo, @Diem)";
+                // Tạo object từ class SqlConnection (dùng để quản lý kết nối đến Database Server)
+                using (SqlConnection connection = new SqlConnection(connectionstring))
+                {
+                    // Tạo object từ class SqlCommand (dùng để quản lý việc thực thi câu lệnh)
+                    using (SqlCommand command = new SqlCommand(queryString, connection))
+                    {
+                        try
+                        {
+                            // Mở kết nối đến Database Server
+                            connection.Open();
+                            // Truyền dữ liệu vào đúng tham số
+                            command.CommandType = CommandType.Text;
+                            command.Parameters.AddWithValue("@TenBang", txtbang.Text);
+                            command.Parameters.AddWithValue("@TenDoi", txtDoiB.Text);
+                            command.Parameters.AddWithValue("@TenDoiDoiThu", txtDoiA.Text);
+                            command.Parameters.AddWithValue("@Tiso", tisoluot1);
+                            command.Parameters.AddWithValue("@SoTranDaThiDau", 1);
+                            command.Parameters.AddWithValue("@HieuSo", hieusoA);
+                            command.Parameters.AddWithValue("@Diem", diemA);
+                            // Thực thi câu lệnh
+                            int effect = command.ExecuteNonQuery();
+                            // Ngắt kết nối đến Database Server
+                            connection.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            // Hiển thị thông báo nếu có lỗi
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+        private void btnluu_Click_1(object sender, EventArgs e)
+        {
+            themlanmot();
+            themlanhai();
+        }
+
+        private void btnxemkq_Click(object sender, EventArgs e)
+        {
+            FrmKetqua frm = new FrmKetqua();
+            frm.ShowDialog();
+            this.Close();
+
+        }
+
+
     }
 }
